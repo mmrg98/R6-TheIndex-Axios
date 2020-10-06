04c6afaec4f22d4348e8328958bd19c97871fc19
mmrg98 <mariam.elgaria98@gmail.com>
2020-10-07 01:04:30 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React, { useState } from "react";
+import React, {useState, useEffect} from 'react';
 
-import authors from "./data.js";
+import axios from "axios";
+import ReactLoading from 'react-loading';
+
+//import authors from "./data.js";
 // Components
 import Sidebar from "./Sidebar";
 import AuthorList from "./AuthorList";
 import AuthorDetail from "./AuthorDetail";
 
 const App = () => {
+  const [authors, setAuthors] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchAuthors = async () =>{
+    try{
+    const response = await axios.get(`https://the-index-api.herokuapp.com/api/authors/`);
+    const fetchedAuthors = response.data
+    setAuthors(fetchedAuthors)
+    setLoading(false)
+    }catch(error){
+    console.log(error)
+    }
+    }
+    useEffect(()=> {
+      fetchAuthors();
+    }, [])
+
   const [currentAuthor, setCurrentAuthor] = useState(null);
 
-  const selectAuthor = author => setCurrentAuthor(author);
+  const selectAuthor = async (author) =>{
+    setLoading(true)
+    try{
+    const response = await axios.get(`https://the-index-api.herokuapp.com/api/authors/${author.id}/`)
+    const fetchedAuthors = response.data
+    setCurrentAuthor(fetchedAuthors)
+    setLoading(false)
+    }catch(error){
+    console.log(error)
+    }
+    }
+
+  //const selectAuthor = author => setCurrentAuthor(author);
 
   const unselectAuthor = () => setCurrentAuthor(null);
 
   const getContentView = () => {
+    if(!loading){
     if (currentAuthor) {
       return <AuthorDetail author={currentAuthor} />;
     } else {
       return <AuthorList authors={authors} selectAuthor={selectAuthor} />;
     }
+    
+  }
+  return <div><ReactLoading type="spin" color="white" height={300} width={100} /></div>
+  
   };
 
   return (
--- src/AuthorList.js
+++ src/AuthorList.js
@@ -5,6 +5,8 @@ import AuthorCard from "./AuthorCard";
 import SearchBar from "./SearchBar";
 
 const AuthorList = props => {
+  const [authors, setAuthors] = useState([]);
+  
   const [query, setQuery] = useState("");
 
   const filterAuthors = query => setQuery(query);
@@ -16,9 +18,10 @@ const AuthorList = props => {
   );
   const authorCards = filteredAuthors.map(author => (
     <AuthorCard
-      key={author.first_name + author.last_name}
+      key={author.id}
       author={author}
       selectAuthor={props.selectAuthor}
+      fetchAuthors={props.fetchAuthors}
     />
   ));
 
